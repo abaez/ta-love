44eafdeb7077f9481b382de0dee0e036c55fa23f
Alejandro Baez <Alejan.Baez@gmail.com>
2015-01-12 18:49:32 -0500

    added init.lua

---
+++ init.lua
@@ -0,0 +1,22 @@
+--- default initializer
+-- @author Alejandro Baez <alejan.baez@gmail.com>
+-- @copyright 2015
+-- @license MIT (see LICENSE)
+-- @module init
+
+-- run love2d project
+textadept.run.build_commands["main.lua"] = "love ."
+
+if type(snippets) == 'table' then
+  snippets.love = snippets.lua
+end
+
+events.connect(events.LEXER_LOADED, function (lang)
+  if lang ~= 'love' then return end
+
+  buffer.tab_width = 4
+  buffer.use_tabs = false
+  buffer.edge_column = 79
+end)
+
+return {}
